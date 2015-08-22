1b428efb5c1eadb4fecd82e43c8f6c6b43f73e5d
guidoarnone <guido.arnone@mercadolibre.com>
2015-08-22 04:05:52 -0300

    added preliminar vision checks, still not working

---
+++ Assets/Scripts/Enemies/EnemyVision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyVision : MonoBehaviour {
+
+	void OnCollisionEnter(Collision c) {
+		if (c.collider.tag == GameManager.CHARACTER_TAG) {
+			GameManager.getInstance().characterWasSeen();
+		}
+	}
+
+}
