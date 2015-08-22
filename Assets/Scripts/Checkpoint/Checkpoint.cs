1169090459f5e4e6c3fe8f8f9703b8978695b124
guidoarnone <guido.arnone@mercadolibre.com>
2015-08-22 16:42:24 -0300

    Added checkpoint logic

---
+++ Assets/Scripts/Checkpoint/Checkpoint.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour {
+
+	private bool activated;
+
+	void Awake() {
+		activated = false;
+	}
+
+	void OnTriggerEnter(Collider c) {
+		if (c.tag == GameManager.CHARACTER_TAG) {
+			activated = true;
+		}
+	}
+
+	public void activate() {
+		activated = true;
+	}
+
+	public bool hasBeenActivated() {
+		return activated;
+	}
+
+}
